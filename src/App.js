acec83a36d0e620feeb0c9c89842f1548465002e
DESKTOP-7LN1193\niraj <nirajdahal09@gmail.com>
2022-12-07 04:16:31 +1100

    introduced reducer function

--- src/App.js
+++ src/App.js
@@ -11,28 +11,34 @@ import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound';
 
+//import provider
+import { GithubProvider } from './context/github/GithubContext'
+
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
-        <main className='container mx-auto px-3 pb-12'>
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
+          <main className='container mx-auto px-3 pb-12'>
 
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
 
-            <Route path='/notfound' element={<NotFound />} />
+              <Route path='/notfound' element={<NotFound />} />
 
-            <Route path='/*' element={<NotFound />} />
+              <Route path='/*' element={<NotFound />} />
 
-          </Routes>
+            </Routes>
 
-        </main>
+          </main>
 
-        <Footer />
-      </div>
-    </Router>
+          <Footer />
+        </div>
+      </Router>
+
+    </GithubProvider>
 
   );
 }
